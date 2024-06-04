ab23bdd9962a86c1fed85ebcfb220261e71c7de9
thaliawoods <thaliadelano@gmail.com>
2024-06-04 18:13:49 +0200

    Etape2

---
+++ script.js
@@ -0,0 +1,13 @@
+// Étape 1 : organisation de dossiers
+
+// Étape 2 : premier code
+
+let message = "Bonjour !";
+
+console.log(message)
+
+let firstname = "Love";
+
+message = "Bonjour " + firstname + " !";
+
+console.log(message)
